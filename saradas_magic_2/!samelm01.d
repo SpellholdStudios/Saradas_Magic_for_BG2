0370875c3dbf1e59f2a40e2bac3c391b02a5a688
GwendolyneFreddy <gwendolyne.freddy@gmail.com>
2021-07-05 18:12:49 +0200

    TRAified !samelm01.d, even if it is not installed!!!

--- saradas_magic_2/!samelm01.d
+++
@@ -1,5 +0,0 @@
-
-IF ~InParty("!Sartob")~ THEN BEGIN Elmin01
-    SAY ~Greetings, old friend. Ha! Fancy meeting you here. And I see a familiar face among your companions. Glad to see you again, wise Saradas.~  [25ELM06]
-    IF ~~ EXTERN Sarad25j Elmin02
-  END
---
+++ saradas_magic_2/dlg/!samelm01.d
@@ -0,0 +1,6 @@
+BEGIN ~!Samelm01~
+
+IF ~InParty("!Sartob")~ THEN BEGIN Elmin01
+    SAY @0
+    IF ~~ THEN EXTERN Sarad25j Elmin02
+END
